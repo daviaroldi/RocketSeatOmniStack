4a0677b30fe3e42cf422f2f43d012605e47992af
Davi Ritter Aroldi <davi.aroldi@AGROBRASILSEGUROS.COM.BR>
2019-10-03 13:38:18 -0300

    First

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
+// GET, POST, PUT, DELETE
+
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar o corpo da requisição (para crição e edição)
+
+app.use(express.json());
+app.use(routes);
+
+
+app.listen(3333);
