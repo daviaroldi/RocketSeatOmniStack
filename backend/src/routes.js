1b2c5413106e3ce85778a3615ce56bf6e46ed977
Davi Ritter Aroldi <davi.aroldi@AGROBRASILSEGUROS.COM.BR>
2019-10-17 15:15:29 -0300

    adicionada rota de dashboard e criado model de reserva de spot juntamente com as rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,11 +3,23 @@ const multer = require('multer')
 const uploadConfig = require('./config/upload')
 const SessionController = require('./controllers/SessionController')
 const SpotController = require('./controllers/SpotController')
+const DashboardController = require('./controllers/DashboardController')
+const BookingController = require('./controllers/BookingController')
 
 const routes = express.Router();
 const upload = multer(uploadConfig)
 
+//Sessions
 routes.post('/sessions', SessionController.store);
+
+//Spots
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+routes.get('/spots', SpotController.index);
+
+//Dashboard
+routes.get('/dashboard', DashboardController.show);
+
+//Booking
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes;
